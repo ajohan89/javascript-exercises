de2b74f8b23a28b031ecb421576b575b52d250a2
Aaron Johansen <johansen89@gmail.com>
2023-07-17 10:34:34 -0600

    Finished celsius and fahrenheit conversion program.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const solution = (fahrenheit - 32) / 1.8;
+  const fSolution =  solution.toFixed(1);
+  if (solution % 1 === 0){return parseInt(solution);}
+  if (solution % 1 !== 0){return parseFloat(fSolution);}
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  const solution = (celsius * 1.8) + 32;
+  const cSolution = solution.toFixed(1);
+  if (solution % 1 === 0){return parseInt(solution);}
+  if (solution % 1 !== 0){return parseFloat(cSolution);}
 };
 
 // Do not edit below this line
