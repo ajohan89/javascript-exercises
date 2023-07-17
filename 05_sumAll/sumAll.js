004f21409c349d3cdda2934ca150013e132c2fe4
Aaron Johansen <johansen89@gmail.com>
2023-07-16 20:28:38 -0600

    finished sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
 
+const sumAll = function(min,max) {
+            
+    let sum = 0;
+
+    if (min > max){
+        [min, max] = [max, min];
+    }
+    if (typeof min !== "number" || typeof max !== "number"){return "ERROR";}
+    if (min<0 || max<0){return "ERROR";}
+    for(i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
+
+
+            
+        
 };
 
 // Do not edit below this line
