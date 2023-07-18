b45bbaaa3b6dd22dab8214ee3a8196117041d065
Aaron Johansen <johansen89@gmail.com>
2023-07-18 16:11:53 -0600

    finshed calculator program, I learned a lot more about for loops, arrays, methods and factorials

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -25,12 +25,26 @@ const multiply = function(...x) {
         
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+    return Math.pow(x,y);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0 || num === 1){
+    return 1;
+  }
+  for(var i = num - 1; i>=1; i--){
+        num *= i;
+    } return num;
+    /* 
+                    num      var i = num - 1       num *= i         i--       i >= 1?
+    1st iteration:   5           4 = 5 - 1         20 = 5 * 4        3          yes   
+    2nd iteration:  20           3 = 4 - 1         60 = 20 * 3       2          yes
+    3rd iteration:  60           2 = 3 - 1        120 = 60 * 2       1          yes  
+    4th iteration: 120           1 = 2 - 1        120 = 120 * 1      0          no             
+    5th iteration: 120               0                120
+    End of the FOR loop 
+    */
 };
 
 // Do not edit below this line
