4db620256a858eaac88080444e1000511d5b522c
Aaron Johansen <johansen89@gmail.com>
2023-07-19 21:34:44 -0600

    this is using a recursive approach to solving the fibonacci placement

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+function fibonacci(x) {
+    if(x < 0) {
+        return 'OOPS';
+    }
+    if(x < 2) {
+        return parseInt(x);
+    }
+    else {
+        for(let i = 0; i < x; i++) {
+            solution = fibonacci(x-1) + fibonacci(x - 2);
+            return solution;
+        }
+    }
+}
 
-};
+// take nth term input from the user
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
