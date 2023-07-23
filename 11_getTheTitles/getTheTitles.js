c77a8a321e181556a2062dcb62d7cd892056f5af
Aaron Johansen <johansen89@gmail.com>
2023-07-22 19:20:07 -0600

    Finished 09_getTheTitles program using the arrow function and map method.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+       
+        return books.map(book => book.title);
 };
+//The map method is called on the books array. 
+//The map method is used to create a new array by applying a callback 
+//function to each element of the original array and returning the results.
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
