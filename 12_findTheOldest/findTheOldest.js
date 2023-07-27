c28119fbca027de1b9c52c4afc550a47e235b7c2
Aaron Johansen <johansen89@gmail.com>
2023-07-26 20:29:36 -0600

    Done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(name, yearOfBirth, yearOfDeath) {
+    let people = name, yearOfBirth, yearOfDeath;
+    let oldestPerson = yearOfDeath - yearOfBirth;
+    
+    
+    //assume the first person is the oldest
+    for (let i = 0; i < people.length; i++) {
+        const currentAge = people.yearOfDeath[i] - people.yearOfBirth[i];
+        const oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+    
+        if (currentAge > oldestAge) {
+          oldestPerson = currentPerson;
+        }
+      }
+    
+      return oldestPerson;
+    }
 
 // Do not edit below this line
 module.exports = findTheOldest;
